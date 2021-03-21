75de5d301554d1054b90e13392f6d93af6d91438
henrique oliveira <henrique_trind@hotmail.com>
2021-03-21 17:06:36 -0300

    refactor home to use mappers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,9 @@ import Home, { HomeTemplateProps } from 'templates/Home';
 
 import { initializeApollo } from 'utils/apollo';
 import { QUERY_HOME } from 'graphql/queries/home';
-import {
-	QueryHome,
-	QueryHome_freeGames,
-	QueryHome_newGames,
-	QueryHome_sections_popularGames_games,
-	QueryHome_upcomingGames,
-} from 'graphql/generated/QueryHome';
+import { QueryHome } from 'graphql/generated/QueryHome';
+
+import { highlightMapper, gamesMapper, bannerMapper } from 'utils/mappers';
 
 export default function Index(props: HomeTemplateProps) {
 	return <Home {...props} />;
@@ -23,72 +19,21 @@ export async function getStaticProps() {
 		query: QUERY_HOME,
 	});
 
-	const getGames = (
-		gamesArray:
-			| QueryHome_newGames[]
-			| QueryHome_freeGames[]
-			| QueryHome_upcomingGames[]
-			| QueryHome_sections_popularGames_games[]
-	) => {
-		return gamesArray.map((game) => ({
-			title: game.name,
-			img: `http://localhost:1337${game.cover?.url}`,
-			price: game.price,
-			slug: game.slug,
-			developer: game.developers[0].name,
-		}));
-	};
-
 	return {
 		props: {
 			revalidate: 60,
-			banners: banners.map((banner) => ({
-				img: `http://localhost:1337${banner.image?.url}`,
-				title: banner.title,
-				subtitle: banner.subtitle,
-				buttonLabel: banner.button?.label,
-				buttonLink: banner.button?.link,
-				...(banner.ribbon && {
-					ribbonText: banner.ribbon?.text,
-					ribbonColor: banner.ribbon?.color,
-					ribbonSize: banner.ribbon?.size,
-				}),
-			})),
+			banners: bannerMapper(banners),
 			newGamesTitle: sections?.newGames?.title,
-			newGames: getGames(newGames),
+			newGames: gamesMapper(newGames),
 			mostPopularGamesTitle: sections?.popularGames?.title,
-			mostPopularHighlight: {
-				title: sections?.popularGames?.highlight?.title,
-				subtitle: sections?.popularGames?.highlight?.subtitle,
-				buttonLabel: sections?.popularGames?.highlight?.buttonLabel,
-				buttonLink: sections?.popularGames?.highlight?.buttonLink,
-				alignment: sections?.popularGames?.highlight?.alignment,
-				floatImage: `http://localhost:1337${sections?.popularGames?.highlight?.floatImage?.url}`,
-				backgroundImage: `http://localhost:1337${sections?.popularGames?.highlight?.background?.url}`,
-			},
-			mostPopularGames: getGames(sections!.popularGames!.games),
+			mostPopularHighlight: highlightMapper(sections!.popularGames?.highlight),
+			mostPopularGames: gamesMapper(sections!.popularGames!.games),
 			upcomingGamesTitle: sections?.upcomingGames?.title,
-			upcomingGames: getGames(upcomingGames),
-			upcomingHighlight: {
-				title: sections?.upcomingGames?.highlight?.title,
-				subtitle: sections?.upcomingGames?.highlight?.subtitle,
-				buttonLabel: sections?.upcomingGames?.highlight?.buttonLabel,
-				buttonLink: sections?.upcomingGames?.highlight?.buttonLink,
-				alignment: sections?.upcomingGames?.highlight?.alignment,
-				floatImage: `http://localhost:1337${sections?.upcomingGames?.highlight?.floatImage?.url}`,
-				backgroundImage: `http://localhost:1337${sections?.upcomingGames?.highlight?.background?.url}`,
-			},
+			upcomingGames: gamesMapper(upcomingGames),
+			upcomingHighlight: highlightMapper(sections!.upcomingGames?.highlight),
 			freeGamesTitle: sections?.freeGames?.title,
-			freeGamesHighlight: {
-				title: sections?.freeGames?.highlight?.title,
-				subtitle: sections?.freeGames?.highlight?.subtitle,
-				buttonLabel: sections?.freeGames?.highlight?.buttonLabel,
-				buttonLink: sections?.freeGames?.highlight?.buttonLink,
-				alignment: sections?.freeGames?.highlight?.alignment,
-				floatImage: `http://localhost:1337${sections?.freeGames?.highlight?.floatImage?.url}`,
-				backgroundImage: `http://localhost:1337${sections?.freeGames?.highlight?.background?.url}`,
-			},
-			freeGames: getGames(freeGames),
+			freeGamesHighlight: highlightMapper(sections!.freeGames?.highlight),
+			freeGames: gamesMapper(freeGames),
 		},
 	};
 }
---
+++ src/utils/mappers.ts
@@ -0,0 +1,59 @@
+import { QueryGames_games } from 'graphql/generated/QueryGames';
+import {
+	QueryHome_banners,
+	QueryHome_sections_popularGames_highlight,
+	QueryHome_sections_upcomingGames_highlight,
+	QueryHome_sections_freeGames_highlight,
+} from 'graphql/generated/QueryHome';
+
+export const bannerMapper = (banners: QueryHome_banners[] | null) => {
+	return (
+		banners &&
+		banners.map((banner) => ({
+			img: `http://localhost:1337${banner.image?.url}`,
+			title: banner.title,
+			subtitle: banner.subtitle,
+			buttonLabel: banner.button?.label,
+			buttonLink: banner.button?.link,
+			...(banner.ribbon && {
+				ribbonText: banner.ribbon?.text,
+				ribbonColor: banner.ribbon?.color,
+				ribbonSize: banner.ribbon?.size,
+			}),
+		}))
+	);
+};
+
+export const highlightMapper = (
+	highlight:
+		| QueryHome_sections_popularGames_highlight
+		| QueryHome_sections_upcomingGames_highlight
+		| QueryHome_sections_freeGames_highlight
+		| null
+		| undefined
+) => {
+	return (
+		highlight && {
+			title: highlight?.title,
+			subtitle: highlight.subtitle,
+			buttonLabel: highlight.buttonLabel,
+			buttonLink: highlight.buttonLink,
+			alignment: highlight.alignment,
+			floatImage: `http://localhost:1337${highlight.floatImage?.url}`,
+			backgroundImage: `http://localhost:1337${highlight.background?.url}`,
+		}
+	);
+};
+
+export const gamesMapper = (gamesArray: QueryGames_games[] | null) => {
+	return (
+		gamesArray &&
+		gamesArray.map((game) => ({
+			title: game.name,
+			img: `http://localhost:1337${game.cover?.url}`,
+			price: game.price,
+			slug: game.slug,
+			developer: game.developers[0].name,
+		}))
+	);
+};
